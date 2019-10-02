134e79dd8b20648c56bf0a7938a8cc9e3e16a3c6
ericlau00 <sharkguy418@gmail.com>
2019-10-02 16:42:24 -0400

    drive to pos

---
+++ opmode/auton/Basic.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.Drivetrain;
+
+@Autonomous(name = "Basic")
+public class Basic extends LinearOpMode {
+    Drivetrain drivetrain = new Drivetrain(this);
+
+    public void runOpMode() {
+        drivetrain.init(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        drivetrain.driveToPos(5);
+    }
+}
