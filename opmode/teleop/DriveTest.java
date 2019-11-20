dc29afa9f795644ce15d0797776294970b23b0e8
Lucas Lee <lucasclee11@gmail.com>
2019-11-20 16:52:05 -0500

    Corner claw code

--- opmode/teleop/DriveTest.java
+++ opmode/teleop/DriveTest.java
@@ -10,7 +10,7 @@ import org.firstinspires.ftc.teamcode.hardware.Drivetrain;
 @TeleOp(name="Drive", group="Teleop")
 public class DriveTest extends LinearOpMode {
 
-    double servo0Position;
+    double servo0Position = 0.2;
     double servo1Position;
     double servo2Position;
     double servo3Position;
@@ -31,41 +31,28 @@ public class DriveTest extends LinearOpMode {
 
         waitForStart();
 
+
         while(opModeIsActive()) {
             telemetry.addData("Status", drive.servo0.getPosition());
-
-//            servo1Position = 0;
-//            servo2Position = 0;
-//            servo3Position = 0;
-//            servo4Position = 0;
 
             double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
             double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
             double rightX = gamepad1.right_stick_x;
 
-          if(gamepad1.a) {
-                servo0Position = .8;
-                drive.servo0.setDirection(Servo.Direction.REVERSE);
+            drive.servo0.setDirection(Servo.Direction.FORWARD);
 
+
+
+          if(gamepad1.a) {
+              servo0Position = 0.2;
 
             }
           if(gamepad1.b){
-              servo0Position = 0.25;
+              drive.servo0.setDirection(Servo.Direction.REVERSE);
+              servo0Position = 0.6;
           }
-            drive.servo0.setPosition(servo0Position);
-//            if (gamepad1.b) {
-//                servo1Position = 0.5;
-//            }
-//            if(gamepad1.x){
-//                servo2Position = 0.5;
-//            }
-//            if(gamepad1.y){
-//                servo3Position = 0.5;
-//            }
 
-//            drive.servo0.setPosition(servo0Position);
-//            drive.servo2.setPosition(servo2Position);
-//            drive.servo3.setPosition(servo3Position);
+          drive.servo0.setPosition(servo0Position);
 
             telemetry.addData("r", r);
             telemetry.addData("robotAngle", robotAngle);
@@ -73,8 +60,9 @@ public class DriveTest extends LinearOpMode {
             telemetry.addData("stickx", gamepad1.right_stick_x);
             telemetry.addData("sticky", gamepad1.right_stick_y);
             telemetry.addData("bbutton", gamepad1.b);
-            telemetry.addData("servoPosition", drive.servo0.getPosition());
-            telemetry.addData("servoPosition", drive.servo0.getDirection());
+            telemetry.addData("LastGivenDirection", drive.servo0.getPosition());
+            telemetry.addData("servoDirection", drive.servo0.getDirection());
+            telemetry.addData("servoPositionVariable", servo0Position);
             telemetry.addData("abutton", gamepad1.a);
             telemetry.update();
         }
