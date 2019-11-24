72fe8c759911326aca9c97e46d8983f16b99137a
ericlau00 <sharkguy418@gmail.com>
2019-11-24 02:02:25 -0500

    resolve conversations in pr

--- hardware/Claw.java
+++ hardware/Claw.java
@@ -1,21 +1,15 @@
 package org.firstinspires.ftc.teamcode.hardware;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Claw extends Mechanism {
     private Servo grip;
     private Servo swivel;
 
-    public Claw() {
+    public Claw() { }
 
-    }
-
-    public Claw(LinearOpMode opMode) {
-        this.opMode = opMode;
-    }
+    public Claw(LinearOpMode opMode) { this.opMode = opMode; }
 
     public void init(HardwareMap hwMap){
         grip = hwMap.servo.get("grip");
@@ -27,16 +21,12 @@ public class Claw extends Mechanism {
         grip.setPosition(0.2);
     }
 
-    public void close(){
-        grip.setPosition(0.8);
-    }
+    public void close(){ grip.setPosition(0.8); }
 
     public void front() {
         swivel.setPosition(0);
     }
 
-    public void back() {
-        swivel.setPosition(1);
-    }
+    public void back() { swivel.setPosition(1); }
 }
 
